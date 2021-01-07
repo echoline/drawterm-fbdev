9996cae72466dc54920f89a8982b753cd0c2eebd
Linux User <eli@owl.lan>
2021-01-07 12:15:15 -0800

    Initial commit

---
+++ kern/devtab.c
@@ -0,0 +1,41 @@
+#include "u.h"
+#include "lib.h"
+#include "dat.h"
+#include "fns.h"
+#include "error.h"
+
+extern Dev consdevtab;
+extern Dev rootdevtab;
+extern Dev pipedevtab;
+extern Dev ssldevtab;
+extern Dev tlsdevtab;
+extern Dev mousedevtab;
+extern Dev drawdevtab;
+extern Dev ipdevtab;
+extern Dev fsdevtab;
+extern Dev mntdevtab;
+extern Dev lfddevtab;
+extern Dev audiodevtab;
+extern Dev kbddevtab;
+extern Dev cmddevtab;
+extern Dev envdevtab;
+
+Dev *devtab[] = {
+	&rootdevtab,
+	&consdevtab,
+	&pipedevtab,
+	&ssldevtab,
+	&tlsdevtab,
+	&mousedevtab,
+	&drawdevtab,
+	&ipdevtab,
+	&fsdevtab,
+	&mntdevtab,
+	&lfddevtab,
+	&audiodevtab,
+	&kbddevtab,
+	&cmddevtab,
+	&envdevtab,
+	0
+};
+
